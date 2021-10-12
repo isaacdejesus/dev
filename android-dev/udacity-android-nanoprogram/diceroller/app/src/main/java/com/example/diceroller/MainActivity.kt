6176b81d25c0f375a072e090c41afd3d841c0b84
isaacdejesus <isaacderxc@gmail.com>
2021-10-12 14:17:58 -0500

    App with images prior to optimization

--- android-dev/udacity-android-nanoprogram/diceroller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ android-dev/udacity-android-nanoprogram/diceroller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -21,8 +22,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random.nextInt(6)+ 1
-        resultText.text = randomInt.toString()
+
+        //saves image returned by when statement into drawableResource
+        val drawableResource = when (randomInt)
+        {
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        //set/change image displayed by view to values/image obtained from when statement
+        diceImage.setImageResource(drawableResource)
     }
 }
