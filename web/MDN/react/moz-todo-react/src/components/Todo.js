a47109979f4ecbdff57545a69f17b2e7d246c031
isaac <idreyes@uh.edu>
2022-02-26 15:23:20 -0600

    finished client side react MDN

--- web/MDN/react/moz-todo-react/src/components/Todo.js
+++ web/MDN/react/moz-todo-react/src/components/Todo.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
-
+import React, {useEffect, useRef, useState } from "react";
+function usePrevious (value){
+    const ref = useRef();
+    useEffect(()=>{
+        ref.current = value;
+    });
+    return ref.current;
+}
 export default function Todo(props){
     const [isEditing, setEditing] = useState(false);
     const [newName, setNewName] = useState('');
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
+    const wasEditing = usePrevious(isEditing);
+
     function handleChange(e) {
         setNewName(e.target.value);
     }
@@ -23,10 +33,13 @@ export default function Todo(props){
                 className="todo-text" 
                 type="text"
                 value={newName}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                ref = {editFieldRef}
+        />
           </div>
           <div className="btn-group">
-            <button type="button" className="btn todo-cancel" onClick={()=> setEditing(false)}>
+            <button 
+                type="button" className="btn todo-cancel" onClick={()=> setEditing(false)}>
               Cancel
               <span className="visually-hidden">renaming {props.name}</span>
             </button>
@@ -51,7 +64,12 @@ export default function Todo(props){
               </label>
             </div>
             <div className="btn-group">
-              <button type="button" className="btn" onClick={() => setEditing(true)}>
+              <button 
+                type="button" 
+                className="btn" 
+                onClick={() => setEditing(true)}
+                ref={editButtonRef}
+                >
                 Edit <span className="visually-hidden">{props.name}</span>
               </button>
               <button
@@ -64,6 +82,14 @@ export default function Todo(props){
             </div>
         </div>
       );
+    useEffect(()=> {
+        if(!wasEditing && isEditing){
+            editFieldRef.current.focus();
+        }
+        if (wasEditing && !isEditing) {
+            editButtonRef.current.focus();
+        }
+    }, [wasEditing, isEditing]);
 
     return (
         <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li> 
